a8e37cf97d14e2c995dee3543010352a6e8358d3
Janus Nicon <janusnic@gmail.com>
2017-04-16 22:38:36 +0300

    Change unit 9

--- app/Comment.php
+++ app/Comment.php
@@ -3,24 +3,40 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use App\User;
-use App\Article;
 
 class Comment extends Model
 {
-    //
-    public function user()
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'comments';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [
+        'id'
+    ];
+
+    /**
+     * Get the commentable entity that the comment belongs to.
+     */
+    public function commentable()
     {
-        return $this->belongsTo(User::class);
+        return $this->morphTo();
     }
 
     /**
-     * One to Many relation
-     *
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     * Get author
+     * @return mixed
      */
-    public function article()
+    public function author()
     {
-        return $this->belongsTo(Article::class);
+        return $this->morphTo();
     }
+
 }
